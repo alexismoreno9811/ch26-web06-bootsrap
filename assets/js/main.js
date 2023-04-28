bcd16a20492eb2055165a16b2f6df0302718bb8d
Alexis Moreno <alexis.morenotrejo@gmail.com>
2023-04-28 09:29:17 -0600

    feat: calculadora

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,5 +22,26 @@ function resetColorText(){
 function changeName() {
   const name = prompt("¿Cuál es tu nombre?");
   const refName = document.getElementById("change-name");
-  refName.innerHTML="Hola " + name;
+  refName.innerHTML = "Hola " + name;
+}
+
+let valor = (refNum) => {
+  refNum.innerHTML = prompt("¿Cuál numero deseas colocar?");
+}
+
+function recibirNumeros() {
+  return  document.getElementsByClassName("numeros");
+}
+
+function operar(refSimbolo) {
+  const numeros = recibirNumeros();
+  const refSigno = document.getElementById("signo");
+  refSigno.innerHTML = refSimbolo.innerHTML;
+  printRes(eval(numeros[0].innerHTML + refSimbolo.innerHTML + numeros[1].innerHTML));
+}
+
+
+function printRes(resultado) {
+  const refRes = document.getElementById("resultado");
+  refRes.innerHTML = resultado;
 }
